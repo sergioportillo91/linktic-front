5237db833e8195b3d131a21e378ef1cea3aae3fa
Sergio Portillo <sergio.portillo@konexinnovation.com.co>
2024-08-12 15:01:43 -0500

    Titulo: Implementacion auth.guards

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router'
+
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = sessionStorage.getItem('token');
+  const router = inject(Router);
+  if(!token){
+    router.navigate(['login']);
+    return false;
+  }
+  return true;
+};
+
